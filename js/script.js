4e84d0e57ca3521dbb34963df624b63c8f9d6acd
Botir <botirqulturayev01@gmail.com>
2023-08-03 17:42:22 +0500

    first commet

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// QAT`IY REJIM
+"use strict";
+
+// #5 - O`ZGARUVCHILAR
+// let number = 50;
+// const person = {
+//   name: "John",
+// };
+// person.name = "Yusuf";
+// console.log(person);
+
+// // old version
+// console.log(personLast);
+// var personLast = "Sammil";
+
+// #6 - QAT`IY REJIM
+// "use strict" -> bu eski formatdagi kodni yo`q qilib beradi, eski kod yozib qo`ymaslik uchun, yangi rejimda ishlashi uchun. Xato chiqarib beradi;
+
+// number = 10;
+// console.log(number);
+
+// #8 foydalanuvchu bilan muloqat
+// const place = confirm("Are you here?");
+// console.log(place);
+
+// const age = +prompt("how old are you?", "18");
+// console.log(age + 10);
+
+// const favoriteColors = [];
+// favoriteColors[0] = prompt("What`s your favorite color #1?", "");
+// favoriteColors[1] = prompt("What`s your favorite color #2?", "");
+// favoriteColors[2] = prompt("What`s your favorite color #3?", "");
+
+// console.log(favoriteColors);
+
+// #9 INTERPOLATSIYA;
+// const channel = prompt("What is your favorite channel?");
+// console.log("https//youtube.com/" + channel + "/videos");
+// console.log(`https://youtube.com/${channel}`);
+
+const name = "samar";
+const job = "Javascript developer";
+console.log(`User name is ${name}, he is ${job}`);
+
+// #10 OPERATORLAR
+//  == ->qiymat bilan solishtiradi
+// === -> bu ham qiymat ham malumot turi bilan ham solishtiradi
+// console.log(5*5 == 25);
+// console.log(5 * 5 === "25");
